9e45f2c3f44ffb696aeaa1065977f1b29ef9df25
Maxime Oliver <mollivier@solstice-consulting.com>
2014-03-04 16:17:46 -0600

    init

---
+++ SecondScreenDemo/SSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SSAppDelegate.h
+//  SecondScreenDemo
+//
+//  Created by Maxime Ollivier on 3/4/14.
+//  Copyright (c) 2014 SolsticeMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SecondScreenDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SecondScreenDemo
+//
+//  Created by Maxime Ollivier on 3/4/14.
+//  Copyright (c) 2014 SolsticeMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SSAppDelegate class]));
+    }
+}
